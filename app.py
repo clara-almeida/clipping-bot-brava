acdb15cb81e77a66f5ca68b8561efdb5c41b02a2
clara-almeida <clara.almeidagom@gmail.com>
2024-12-03 20:26:36 -0300

    push13

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import requests
 from flask import Flask, render_template, request, redirect, url_for
 from raspador import get_epbr
 from raspador import get_infomoney
-from raspador import get_investing
-from raspador import get_petronoticias
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -14,8 +13,6 @@ app = Flask(__name__) # Cria uma instância do Flask.
 def raspadores():
     run_epbr = get_epbr()
     run_infomoney = get_infomoney()
-    run_investing = get_investing()
-    run_petronoticias = get_petronoticias()
     return render_template('raspadores.html')
 
 
