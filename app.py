d72bf19487aec691c79e562d429b5dbc3498747b
clara-almeida <clara.almeidagom@gmail.com>
2024-12-09 20:17:44 -0300

    push18

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__) # Cria uma instância do Flask.
 def raspadores():
     run_epbr = get_epbr()
     run_infomoney = get_infomoney()
-    #run_investing = get_investing()
+    run_investing = get_investing()
     #run_petronoticias = get_petronoticias()
     return render_template('raspadores.html')
 
